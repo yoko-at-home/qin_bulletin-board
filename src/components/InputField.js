f22e867d6c6a69db97c7d2309b3dd58201f899ee
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-05-05 00:24:07 +0900

    🩹 Inputフィールドもドラッグできるようにしました

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -2,8 +2,12 @@ import React, { useState, useContext } from "react";
 import { db } from '../firebase';
 import randomColor from 'randomcolor';
 import { context } from "../constext/ContextProvider";
+import Draggable from "react-draggable";
+
 
 const InputField = () => {
+  const nodeRef = React.useRef(null);
+
   const [item, setItem] = useState('');
   const { name } = useContext(context);
 
@@ -32,26 +36,28 @@ const InputField = () => {
   };
 
   return (
-    <div className='input__wrapper'>
-      <input
-        value={item}
-        onChange={(e) => setItem(e.target.value)}
-        placeholder='お題を入力'
-        onKeyPress={(e) => keyPress(e)}
-      />
-      <button
-        style={{
-          fontSize: '1rem',
-          color: 'yellow',
-          padding: '5px',
-          height: 'fit-content',
-          border: '3px dotted rgb(255, 251, 0)',
-        }}
-        onClick={newitem}
-      >
-        ENTER
-      </button>
-    </div>
+    <Draggable nodeRef={nodeRef}>
+      <div className="input__wrapper">
+        <input
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+          placeholder="お題を入力"
+          onKeyPress={(e) => keyPress(e)}
+        />
+        <button
+          style={{
+            fontSize: "1rem",
+            color: "yellow",
+            padding: "5px",
+            height: "fit-content",
+            border: "3px dotted rgb(255, 251, 0)",
+          }}
+          onClick={newitem}
+        >
+          ENTER
+        </button>
+      </div>
+    </Draggable>
   );
 };
 
