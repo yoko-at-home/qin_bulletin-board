92f84cf14461c3d9ddf9096a5265f73f7ffa0ef2
takasaki376 <75880059+takasaki376@users.noreply.github.com>
2021-05-04 23:42:33 +0900

    🚩 realtimedatabaseに変更

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,7 +1,13 @@
+<<<<<<< HEAD
 import React, { useState, useEffect, useContext } from "react";
 import Draggable from 'react-draggable';
 import { db } from "../firebase";
 import { context } from "../constext/ContextProvider";
+=======
+import React, { useState, useEffect } from "react";
+import Draggable from "react-draggable";
+import { themeRef } from "../firebase";
+>>>>>>> 5c96331 (🚩 realtimedatabaseに変更)
 
 const Board = () => {
   const { admin } = useContext(context);
@@ -13,15 +19,24 @@ const Board = () => {
     newArr[index].defaultPos = { x: data.x, y: data.y };
     setItems(newArr);
 
+<<<<<<< HEAD
     const themeRef = db.collection("theme").doc(newArr[index].id);
     themeRef.update({
       pojx: newArr[index].defaultPos.x,
       pojy: newArr[index].defaultPos.y,
     });
+=======
+    // const themeRef = db.collection("theme").doc(newArr[index].id);
+    // themeRef.update({
+    //   pojx: newArr[index].defaultPos.x,
+    //   pojy: newArr[index].defaultPos.y,
+    // });
+>>>>>>> 5c96331 (🚩 realtimedatabaseに変更)
   };
 
   const deleteNote = (id) => {
     setItems(items.filter((item) => item.id !== id));
+<<<<<<< HEAD
     db.collection("theme").doc(id).delete();
   };
 
@@ -34,9 +49,38 @@ const Board = () => {
           color: doc.data().color,
           defaultPos: { x: doc.data().pojx, y: doc.data().pojy },
           user: doc.data().user,
+=======
+    // db.collection("theme").doc(id).delete();
+  };
+
+  useEffect(() => {
+    // db.collection('theme').onSnapshot((snapshot) =>
+    //   setItems(
+    //     snapshot.docs.map((doc) => ({
+    //       id: doc.id,
+    //       item: doc.data().item,
+    //       color: doc.data().color,
+    //       defaultPos: { x: doc.data().pojx, y: doc.data().pojy },
+    //        user:doc.data().user,
+    //     }))
+    //   )
+    // );
+    themeRef.on("value", (snapshot) => {
+      const items = snapshot.val();
+      if (items === null) return;
+
+      const entries = Object.entries(items);
+      setItems(
+        entries.map((entry) => ({
+          id: entry.id,
+          item: entry.item,
+          color: entry.color,
+          defaultPos: { x: entry.pojx, y: entry.pojy },
+          user: entry.user,
+>>>>>>> 5c96331 (🚩 realtimedatabaseに変更)
         }))
-      )
-    );
+      );
+    });
   }, []);
 
   return (
@@ -57,6 +101,7 @@ const Board = () => {
               className="box"
             >
               {`${item.item}`}
+<<<<<<< HEAD
               {admin ? (
                 <button
                   className="button"
@@ -66,6 +111,15 @@ const Board = () => {
                   X
                   </button>
               ) : null}
+=======
+              <button
+                className="button"
+                id="delete"
+                onClick={(e) => deleteNote(item.id)}
+              >
+                X
+              </button>
+>>>>>>> 5c96331 (🚩 realtimedatabaseに変更)
             </div>
           </Draggable>
         );
--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,26 +1,33 @@
-import React, { useState, useContext } from "react";
-import { db } from '../firebase';
-import randomColor from 'randomcolor';
+import React, { useContext, useState } from "react";
+import { themeRef } from "../firebase";
+import randomColor from "randomcolor";
 import { context } from "../constext/ContextProvider";
 
 const InputField = () => {
-  const [item, setItem] = useState('');
+  const [item, setItem] = useState("");
   const { name } = useContext(context);
 
   const newitem = () => {
-    if (item.trim() !== '') {
-      db.collection("theme").add({
+    if (item.trim() !== "") {
+      // db.collection('theme').add({
+      //   item: item,
+      //   color: randomColor({ luminosity: 'light' }),
+      //   pojx: 50,
+      //   pojy: -50,
+      // });
+      themeRef.push({
         item: item,
         color: randomColor({ luminosity: "light" }),
         pojx: 50,
         pojy: -50,
         user: name,
       });
+
       //reset item value to empty string
-      setItem('');
+      setItem("");
     } else {
-      alert('お題を入力してください');
-      setItem('');
+      alert("お題を入力してください");
+      setItem("");
     }
   };
 
@@ -32,20 +39,20 @@ const InputField = () => {
   };
 
   return (
-    <div className='input__wrapper'>
+    <div className="input__wrapper">
       <input
         value={item}
         onChange={(e) => setItem(e.target.value)}
-        placeholder='お題を入力'
+        placeholder="お題を入力"
         onKeyPress={(e) => keyPress(e)}
       />
       <button
         style={{
-          fontSize: '1rem',
-          color: 'yellow',
-          padding: '5px',
-          height: 'fit-content',
-          border: '3px dotted rgb(255, 251, 0)',
+          fontSize: "1rem",
+          color: "yellow",
+          padding: "5px",
+          height: "fit-content",
+          border: "3px dotted rgb(255, 251, 0)",
         }}
         onClick={newitem}
       >
