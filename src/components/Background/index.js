ca69b5200721d03ebb3a5f344a603b4ef418df26
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-05-04 16:54:01 +0900

    💄 背景グリッド更新

---
+++ src/components/Background/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./styles.css";
+import Muchan from "../Muchan";
+
+const Background = () => {
+  return (
+    <div>
+      <div className="Bg">
+        <div className="odai">お題</div>
+        <div className="hanashichu">
+          <div className="child">
+            <Muchan />
+            話中のお題
+          </div>
+        </div>
+        <div className="owari">話し終えたお題</div>
+        <div className="mibunrui">未分類</div>
+      </div>
+    </div>
+  );
+};
+
+export default Background;
