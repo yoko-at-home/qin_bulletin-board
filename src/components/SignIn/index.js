deb74e33738bbf81cc5ed60f18bd641c7ee06b05
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-05-06 18:18:24 +0900

    🐛

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -3,7 +3,7 @@ import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
-import Box from "@material-ui/core/Box";
+
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
@@ -38,7 +38,7 @@ function Credit() {
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    marginTop: theme.spacing(8),
+    // marginTop: theme.spacing(8),
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
